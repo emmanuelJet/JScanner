8623ce34bd9784e5c4a92a726933be7fe7eaaa17
Emmanuel Joseph <32677156+jet774@users.noreply.github.com>
2018-10-20 20:02:35 +0100

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <barcode_scan/BarcodeScanPlugin.h>
+#import <path_provider/PathProviderPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [BarcodeScanPlugin registerWithRegistrar:[registry registrarForPlugin:@"BarcodeScanPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+}
+
+@end
---
+++ lib/ui/generate.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'dart:ui';
+import 'package:flutter/rendering.dart';
+
+class GenerateScreen extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => GenerateScreenState();
+}
+
+class GenerateScreenState extends State<GenerateScreen> {
+
+  static const double _topSectionTopPadding = 50.0;
+  static const double _topSectionBottomPadding = 20.0;
+  static const double _topSectionHeight = 50.0;
+
+  GlobalKey globalKey = new GlobalKey();
+  String _dataString = "Hello from this QR";
+  String _inputErrorText;
+  final TextEditingController _textController =  TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('JGenerator'),
+      ),
+      body: _contentWidget(),
+    );
+  }
+
+
+
+  _contentWidget() {
+    final bodyHeight = MediaQuery.of(context).size.height - MediaQuery.of(context).viewInsets.bottom;
+    return  Container(
+      color: const Color(0xFFFFFFFF),
+      child:  Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+              top: _topSectionTopPadding,
+              left: 20.0,
+              right: 10.0,
+              bottom: _topSectionBottomPadding,
+            ),
+            child:  Container(
+              height: _topSectionHeight,
+              child:  Row(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Expanded(
+                    child:  TextField(
+                      controller: _textController,
+                      decoration:  InputDecoration(
+                        hintText: "Enter a custom message",
+                        errorText: _inputErrorText,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child:  FlatButton(
+                      child:  Text("SUBMIT"),
+                      onPressed: () {
+                        setState((){
+                          _dataString = _textController.text;
+                          _inputErrorText = null;
+                        });
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child:  Center(
+              child: RepaintBoundary(
+                key: globalKey,
+                child: QrImage(
+                  data: _dataString,
+                  size: 0.5 * bodyHeight,
+                  onError: (ex) {
+                    print("[QR] ERROR - $ex");
+                    setState((){
+                      _inputErrorText = "Error! Maybe your input value is too long?";
+                    });
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
